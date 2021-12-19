5ec11c0e0ad2e870a381869d08ff7502b58b0fab
Chuxwa <799586940@qq.com>
2021-12-19 14:35:08 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python train.py --img 1024 --batch 4 --epochs 70 --data ./data/GlobalWheat_2021.yaml --cfg ./models/yolov5xse.yaml --name yolov5xse_1024 --device 5 --weights ./yolov5x.pt
+python train.py --img 1024 --batch 4 --epochs 70 --data data/GlobalWheat_2021.yaml --cfg ./models/yolov5x.yaml --name yolov5xse_mixup_1024 --device 1 --weights ./yolov5x.pt --hyp data/hyps/hyp.wheatdetection.yaml
